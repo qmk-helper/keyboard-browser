778e21f891bb8d6c62ae5a8b0be89e171019b801
Buckwich <Buckwich@users.noreply.github.com>
2020-09-05 17:33:29 +0200

    add keyboard parameter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { KeyboardListModule } from './keyboard-list/keyboard-list.module';
-
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, KeyboardListModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    KeyboardListModule,
+    RouterModule.forRoot([]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
