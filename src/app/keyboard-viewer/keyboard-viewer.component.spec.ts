cfeced77d4d22ba93f4819a7bcb1dbc7363201eb
Simon <Buckwich@users.noreply.github.com>
2020-12-12 20:51:38 +0100

    Revert "update"

--- src/app/keyboard-viewer/keyboard-viewer.component.spec.ts
+++ src/app/keyboard-viewer/keyboard-viewer.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { KeyboardViewerComponent } from './keyboard-viewer.component';
 
@@ -6,7 +6,7 @@ describe('KeyboardViewerComponent', () => {
   let component: KeyboardViewerComponent;
   let fixture: ComponentFixture<KeyboardViewerComponent>;
 
-  beforeEach(waitForAsync(() => {
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ KeyboardViewerComponent ]
     })
--- src/app/keyboard-viewer/keymap-renderer/keymap-renderer.component.spec.ts
+++ src/app/keyboard-viewer/keymap-renderer/keymap-renderer.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { KeymapRendererComponent } from './keymap-renderer.component';
 
@@ -6,7 +6,7 @@ describe('KeymapRendererComponent', () => {
   let component: KeymapRendererComponent;
   let fixture: ComponentFixture<KeymapRendererComponent>;
 
-  beforeEach(waitForAsync(() => {
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ KeymapRendererComponent ]
     })
